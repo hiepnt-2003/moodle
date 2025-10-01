a538fb5c26ac7c4124a0b6990f43f0840c8fd7a5
Nguyễn Tiến Hiệp <hiepnt.b21cn344@stu.ptit.edu.vn>
2025-10-01 08:56:58 +0700

    webservice create user

--- local/clonecourse/lang/en/local_clonecourse.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$string['pluginname'] = 'Clone Course';
-$string['clonecourse'] = 'Clone Course';
-$string['clonecourse:view'] = 'View courses in categories';
-$string['clonecourse:manage'] = 'Manage course cloning';
-$string['privacy:metadata'] = 'The Clone Course plugin does not store any personal data.';
---
+++ local/usercreation/lang/en/local_usercreation.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for local_usercreation plugin.
+ *
+ * @package    local_usercreation
+ * @copyright  2025 Your Name <your.email@example.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'User Creation Service';
+$string['usercreation:createuser'] = 'Create users via web service';
+
+// Success messages.
+$string['success_usercreated'] = 'User has been successfully created';
+
+// Error messages.
+$string['error_usernameinuse'] = 'Username "{$a}" is already in use';
+$string['error_emailinuse'] = 'Email "{$a}" is already in use';
+$string['error_requiredfields'] = 'All required fields must be provided';
+$string['error_invalidusername'] = 'Username contains invalid characters. Only letters, numbers, dots, underscores and hyphens are allowed';
+$string['error_invalidemail'] = 'Invalid email format';
+$string['error_passwordrequired'] = 'Password is required when createpassword is false';
+$string['error_passwordweak'] = 'Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, and one digit';
+$string['error_usercreationfailed'] = 'Failed to create user in database';
