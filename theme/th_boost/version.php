b2d94de59e8b87e452f9627fb487f0ee8dadc16e
Nguyễn Tiến Hiệp <hiepnt.b21cn344@stu.ptit.edu.vn>
2025-10-02 14:20:32 +0700

    update theme

--- theme/th_boost/version.php
+++ theme/th_boost/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_th_boost';
-$plugin->version = 2025100200;
+$plugin->version = 2025100201;
 $plugin->requires = 2020110900;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
+$plugin->release = '1.0.';
