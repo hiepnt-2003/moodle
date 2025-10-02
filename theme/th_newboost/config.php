11b715bfc718d048277429ba439dab2bb32354ff
Nguyễn Tiến Hiệp <hiepnt.b21cn344@stu.ptit.edu.vn>
2025-10-02 15:55:12 +0700

    new theme

---
+++ theme/th_newboost/config.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme TH NewBoost config.
+ *
+ * @package   theme_th_newboost
+ * @copyright 2025
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$THEME->name = 'th_newboost';
+$THEME->sheets = [];
+$THEME->editor_sheets = [];
+$THEME->parents = ['boost'];
+$THEME->enable_dock = false;
+$THEME->yuicssmodules = [];
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+$THEME->requiredblocks = '';
+$THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
+$THEME->iconsystem = \core\output\icon_system_fontawesome::class;
+$THEME->haseditswitch = true;
+$THEME->usescourseindex = true;
+
+// Custom layouts for modern design
+$THEME->layouts = [
+    // The site home page.
+    'frontpage' => array(
+        'file' => 'frontpage.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+        'options' => array('nonavbar' => true),
+    ),
+];
+
+// SCSS compilation
+$THEME->scss = function($theme) {
+    return theme_th_newboost_get_main_scss_content($theme);
+};
+
+$THEME->prescsscallback = 'theme_th_newboost_get_pre_scss';
---
+++ theme/th_newboost/settings.php
@@ -0,0 +1,87 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme TH NewBoost settings.
+ *
+ * @package   theme_th_newboost
+ * @copyright 2025
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->fulltree) {
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingth_newboost', get_string('configtitle', 'theme_th_newboost'));
+    $page = new admin_settingpage('theme_th_newboost_general', get_string('generalsettings', 'theme_th_newboost'));
+
+    // Preset.
+    $name = 'theme_th_newboost/preset';
+    $title = get_string('preset', 'theme_th_newboost');
+    $description = get_string('preset_desc', 'theme_th_newboost');
+    $default = 'default.scss';
+
+    $context = context_system::instance();
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($context->id, 'theme_th_newboost', 'preset', 0, 'itemid, filepath, filename', false);
+
+    $choices = [];
+    foreach ($files as $file) {
+        $choices[$file->get_filename()] = $file->get_filename();
+    }
+    $choices['default.scss'] = 'default.scss';
+    $choices['plain.scss'] = 'plain.scss';
+
+    $setting = new admin_setting_configthemepreset($name, $title, $description, $default, $choices, 'th_newboost');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Preset files setting.
+    $name = 'theme_th_newboost/presetfiles';
+    $title = get_string('presetfiles', 'theme_th_newboost');
+    $description = get_string('presetfiles_desc', 'theme_th_newboost');
+
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
+        array('maxfiles' => 20, 'accepted_types' => array('.scss')));
+    $page->add($setting);
+
+    // Brand colour setting.
+    $name = 'theme_th_newboost/brandcolor';
+    $title = get_string('brandcolor', 'theme_th_newboost');
+    $description = get_string('brandcolor_desc', 'theme_th_newboost');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, '#667eea');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    $settings->add($page);
+
+    // Advanced settings.
+    $page = new admin_settingpage('theme_th_newboost_advanced', get_string('advancedsettings', 'theme_th_newboost'));
+
+    // Raw SCSS to include before the content.
+    $setting = new admin_setting_scsscode('theme_th_newboost/scsspre',
+        get_string('rawscsspre', 'theme_th_newboost'), get_string('rawscsspre_desc', 'theme_th_newboost'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Raw SCSS to include after the content.
+    $setting = new admin_setting_scsscode('theme_th_newboost/scss', get_string('rawscss', 'theme_th_newboost'),
+        get_string('rawscss_desc', 'theme_th_newboost'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    $settings->add($page);
+}
