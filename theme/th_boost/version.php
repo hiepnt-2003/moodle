34962ae6b053a18dc64a1a2f538ad1a64c246f55
Nguyễn Tiến Hiệp <hiepnt.b21cn344@stu.ptit.edu.vn>
2025-10-09 11:26:00 +0700

    fix error theme_boost

--- theme/th_boost/version.php
+++ theme/th_boost/version.php
@@ -13,6 +13,4 @@ $plugin->version   = 2025100900;        // YYYYMMDDXX.
 $plugin->requires  = 2019111800;        // Requires Moodle 3.8+ (compatible with 3.9).
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0';
-$plugin->dependencies = [
-    'theme_boost' => 2022041900
-];
+// Theme boost is part of Moodle core, no need to declare dependency.
